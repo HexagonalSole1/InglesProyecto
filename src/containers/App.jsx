4f0a9b6ceb34ed73a89a105ae5a3eaaeb7dcca11
gallegos19 <223204@ids.upchiapas.edu.mx>
2023-05-21 09:53:06 -0600

    estructura

---
+++ src/containers/App.jsx
@@ -0,0 +1,18 @@
+import '../css/App.css'
+import Nav from '../containers/nav'
+import Footer from '../containers/footer'
+import Content from '../containers/upnidosContent'
+function App() {
+
+  return (
+    <>
+      <div>
+      <Nav/>
+      <Content/>
+      <Footer/>
+      </div>
+    </>
+  )
+}
+
+export default App
