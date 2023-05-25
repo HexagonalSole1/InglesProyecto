a01343ee6780d046809aa8f4c3186d7e91decf79
gallegos19 <223204@ids.upchiapas.edu.mx>
2023-05-24 20:27:37 -0600

    Contenido

--- src/components/upnidosContent.jsx
+++ src/components/upnidosContent.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import News from "./news";
+import Style from '../css/upcontent.css'
+import img from '../assets/julio.jpeg'
+import img2 from '../assets/julian.jpeg'
 
 export default function upnidosContent() {
   return (
@@ -7,21 +10,52 @@ export default function upnidosContent() {
       <News
         title="What are your hobbies"
         autor="Julian"
-        text="
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacinia quam id dolor varius, at tempus dolor pellentesque. Aliquam lobortis, mi a commodo pharetra, metus urna ullamcorper massa, nec aliquam nisl est ut mi. Vestibulum vestibulum purus ac turpis hendrerit, ut dictum tortor pellentesque. Pellentesque auctor, lectus id commodo laoreet, mi nisl faucibus turpis, non tristique massa sapien non libero. Nullam sagittis, erat a congue consectetur, turpis tortor consectetur risus, ut semper massa turpis ac tortor. Nullam id lorem ullamcorper, convallis nunc eu, eleifend ipsum. Curabitur id fermentum dolor. Sed sollicitudin, urna id dapibus lobortis, mauris leo ultricies ligula, sed fringilla ipsum urna eu est. Suspendisse non dolor et mauris convallis pellentesque. Integer sem tellus, sodales id elementum non, consequat a nunc. Sed maximus neque a nibh lacinia, a viverra quam lacinia. Mauris vel rutrum ligula. Ut gravida, felis ut convallis facilisis, dolor elit semper neque, ut feugiat ligula lectus id ipsum."
+        text="Well i usually play video games in my house or sometimes i read monwhas"
       />
        <News
         title="What are your hobbies"
-        autor="Julian"
-        text="
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacinia quam id dolor varius, at tempus dolor pellentesque. Aliquam lobortis, mi a commodo pharetra, metus urna ullamcorper massa, nec aliquam nisl est ut mi. Vestibulum vestibulum purus ac turpis hendrerit, ut dictum tortor pellentesque. Pellentesque auctor, lectus id commodo laoreet, mi nisl faucibus turpis, non tristique massa sapien non libero. Nullam sagittis, erat a congue consectetur, turpis tortor consectetur risus, ut semper massa turpis ac tortor. Nullam id lorem ullamcorper, convallis nunc eu, eleifend ipsum. Curabitur id fermentum dolor. Sed sollicitudin, urna id dapibus lobortis, mauris leo ultricies ligula, sed fringilla ipsum urna eu est. Suspendisse non dolor et mauris convallis pellentesque. Integer sem tellus, sodales id elementum non, consequat a nunc. Sed maximus neque a nibh lacinia, a viverra quam lacinia. Mauris vel rutrum ligula. Ut gravida, felis ut convallis facilisis, dolor elit semper neque, ut feugiat ligula lectus id ipsum."
-      />
+        autor="Julio"
+        text="I really like to wathc stremers in spanish fir example Auronplay, Quackity, Roier, LarzRicaldi. Some streams is GTA5 roleplay i like it"      />
        <News
-        title="What are your hobbies"
+        title="Can you play a instrumental music?"
         autor="Julian"
-        text="
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacinia quam id dolor varius, at tempus dolor pellentesque. Aliquam lobortis, mi a commodo pharetra, metus urna ullamcorper massa, nec aliquam nisl est ut mi. Vestibulum vestibulum purus ac turpis hendrerit, ut dictum tortor pellentesque. Pellentesque auctor, lectus id commodo laoreet, mi nisl faucibus turpis, non tristique massa sapien non libero. Nullam sagittis, erat a congue consectetur, turpis tortor consectetur risus, ut semper massa turpis ac tortor. Nullam id lorem ullamcorper, convallis nunc eu, eleifend ipsum. Curabitur id fermentum dolor. Sed sollicitudin, urna id dapibus lobortis, mauris leo ultricies ligula, sed fringilla ipsum urna eu est. Suspendisse non dolor et mauris convallis pellentesque. Integer sem tellus, sodales id elementum non, consequat a nunc. Sed maximus neque a nibh lacinia, a viverra quam lacinia. Mauris vel rutrum ligula. Ut gravida, felis ut convallis facilisis, dolor elit semper neque, ut feugiat ligula lectus id ipsum."
-      />
+        text="not, i dont like playin instrument, i try playing guittar but i dont have a talent music"      />
+      <div className="contenedor-img">
+      <img src={img} alt="" className="imagen"/>
+      </div>
+       <News
+        title="Can you play a instrumental music?"
+        autor="Julio"
+        text="Yes, I play some flute and drum every january for traditional fest of Chiapa de Corzo and other some ocations "     />
+       <News
+        title="What else do you enjoy doing in your free-time?"
+        autor="Julian"
+        text="When I am reading manejas the category that more read is Isekais or romance the best manejas is TBATE ,Eleeced,sólo leveling or player"      />
+       <News
+        title="What else do you enjoy doing in your free-time?"
+        autor="Julio"
+        text="I read mistery books, i play videogames like Valorant, Rocket Ligue, Fortnite. And learnig new program languages"       />
+        <div className="contenedor-img">
+         <img src={img2} alt="" className="imagen"/>
+         </div>
+        <News
+        title="Are you good at sport?"
+        autor="Julian"
+        text="Yeah, i´m good in Ea sport but in really sports i´m good the only sport tha i play is volleyball"       />
+        <News
+        title="Are you good at sport?"
+        autor="Julio"
+        text="I consider myself good at basquetball and volleyball, i usually play everyweekends with my sister. "       />
+       <News
+        title="Are you interested in leaning new things?"
+        autor="Julian"
+        text="Yes i usually learn new heghmologies in programing"       />
+       <News
+        title="Are you interested in leaning new things?"
+        autor="Julio"
+        text="yes, i like the idea of learning new things in my free time.
+        I´d like like to learn Mountain climbing or inclusive new hobbies"/>
+                  
     </div>
   );
 }
